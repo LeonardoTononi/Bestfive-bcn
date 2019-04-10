03538a7f6eec9d7762c947cbbd1df39200ed3643
Leonardo Tononi <easaaa@protonmail.com>
2019-04-10 15:03:54 +0200

    Add effect back icon

---
+++ javaScript/reviewEffects.js
@@ -0,0 +1,11 @@
+const backIcon_div = document.getElementById('back');
+
+window.addEventListener('scroll', () => {
+  const scrolled = window.scrollY;
+  
+  if (scrolled > 90) {
+    backIcon_div.style.display = "inline-block";
+  } else {
+    backIcon_div.style.display = "none";
+  }
+});
