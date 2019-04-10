4586f9e6d28cb28192e5dd80a044085297b29323
Leonardo Tononi <easaaa@protonmail.com>
2019-04-10 10:55:57 +0200

    Fixed style cookie banner

--- javaScript/cookies.js
+++ javaScript/cookies.js
@@ -1,11 +1,16 @@
-window.addEventListener("load", function(){
+
+window.addEventListener("load", function () {
   window.cookieconsent.initialise({
     "palette": {
       "popup": {
-        "background": "#000"
+        "background": "#000000",
+        "text": "#ffffff"
       },
       "button": {
-        "background": "#ffffff"
+        "background": "#8b8d8e",
+        "text": "#ffffff"
       }
-    }
-  })});
+    },
+    "theme": "edgeless"
+  });
+});
