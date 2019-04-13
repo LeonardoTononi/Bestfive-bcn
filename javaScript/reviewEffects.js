0090f5132179be2f55e2b4c87a0316feefbb6125
Leonardo Tononi <easaaa@protonmail.com>
2019-04-13 19:34:47 +0200

    Remove scroll effect and change color text

--- javaScript/reviewEffects.js
+++ javaScript/reviewEffects.js
@@ -2,7 +2,6 @@ const backIcon_div = document.getElementById('back');
 
 window.addEventListener('scroll', () => {
   const scrolled = window.scrollY;
-  
   if (scrolled > 90) {
     backIcon_div.style.display = "inline-block";
   } else {
--- javaScript/scrollEffects.js
+++ javaScript/scrollEffects.js
@@ -1,9 +1,9 @@
 // EFFECT PAGE
 
-window.sr = ScrollReveal();
+/* window.sr = ScrollReveal();
 sr.reveal('.place', {
   duration: 1000,
   origin: 'bottom',
   distance: '100px',
   viewFactor: 0.2
-});
+}); */
