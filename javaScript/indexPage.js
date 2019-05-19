e3b57d929551eb38666572fb1d5523f6f7a935f9
Leonardo Tononi <easaaa@protonmail.com>
2019-05-19 21:35:55 +0200

    Format css into scss

--- javaScript/indexPage.js
+++ javaScript/indexPage.js
@@ -23,16 +23,13 @@ window.addEventListener('scroll', () => {
 
 // BTN LANGUAGES
 const btnLanguage_a = document.querySelector('.btn-lang');
-const languagesList_div = document.getElementById('languages-section');
-const cleanBtn_a = document.getElementById('clean-lang');
+const languagesList_div = document.querySelector('.languages');
 
 btnLanguage_a.addEventListener('click', function () {
   if (languagesList_div.style.display == "none") {
-    /* languagesList_div.style.opacity = "1"; */
     languagesList_div.style.display = "inline";
     cleanBtn_a.addEventListener('click', function () {
       languagesList_div.style.display = "none";
-      /* languagesList_div.style.opacity = "0"; */
     });
   } else {
     languagesList_div.style.display = "none";
