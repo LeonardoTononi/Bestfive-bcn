e72fa0ef0b8ae876daf25dc7c438f96e2e366df7
Leonardo Tononi <easaaa@protonmail.com>
2019-05-23 12:41:16 +0200

    Change css into scss in index and review

--- javaScript/indexPage.js
+++ javaScript/indexPage.js
@@ -6,21 +6,6 @@ window.addEventListener('load', function () {
   }, 1000);
 });
 
-//SCROLL APP CHANGE BACKGROUND
-const scrollDown_div = document.querySelector('.downScroll');
-window.addEventListener('scroll', () => {
-  const scrolled = window.scrollY;
-  if (scrolled > 5) {
-    scrollDown_div.style.background = "#fff";
-    scrollDown_div.style.boxShadow = "2px 2px 16px -8px #484848";
-    /* scrollDown_div.style.borderBottom = ".2px solid #484848"; */
-  } else {
-    scrollDown_div.style.background = "transparent";
-    scrollDown_div.style.boxShadow = "none";
-    /* scrollDown_div.style.borderBottom = "none"; */
-  }
-});
-
 // BTN LANGUAGES
 const btnLanguage_a = document.querySelector('.btn-lang');
 const languagesList_div = document.querySelector('.languages');
@@ -58,3 +43,17 @@ closeBtn_div.addEventListener('click', function () {
     footer.style.display = "inline";
   }
 });
+
+
+//SCROLL APP CHANGE BACKGROUND
+/* const scrollDown_div = document.querySelector('.downScroll');
+window.addEventListener('scroll', () => {
+  const scrolled = window.scrollY;
+  if (scrolled > 5) {
+    scrollDown_div.style.background = "#fff";
+    scrollDown_div.style.boxShadow = "2px 2px 16px -8px #484848";
+  } else {
+    scrollDown_div.style.background = "transparent";
+    scrollDown_div.style.boxShadow = "none";
+  }
+}); */
