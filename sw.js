4849c8247f4091345a6dd1cf60b971e28c16249a
Leonardo Tononi <easaaa@protonmail.com>
2019-06-08 15:48:08 +0200

    Added dynamic caching

--- sw.js
+++ sw.js
@@ -1,45 +1,54 @@
-const staticCacheName = 'site-static';
+const staticCacheName = 'site-static:v5';
+const dynamicCacheName = 'site-dynamic:v2';
 const assets = [
   '/',
   '/index.html',
-  '/en/about.html',
-  '/en/howto.html',
-  '/en/map.html',
-  '/en/review/cheapEats.html',
-  '/img',
+  '/img/barcelona-city.png',
+  '/img/violetHand.png',
   '/javaScript/app.js',
   '/javaScript/footer.js',
   '/javaScript/indexPage.js',
-  '/javaScript/map.js',
-  '/javaScript/howto.js',
-  '/stylesheets/css',
+  '/stylesheets/css/index.css',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
   'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
 
-// Install service worker
+// install event
 self.addEventListener('install', evt => {
-  // console.log('service worker has been installed');
+  //console.log('service worker installed');
   evt.waitUntil(
-    // "caches" is a global read-only variable, which is an instance of CacheStorage
-    caches.open(staticCacheName).then(cache => {
+    caches.open(staticCacheName).then((cache) => {
       console.log('caching shell assets');
       cache.addAll(assets);
     })
   );
 });
 
-// Activate service worker 
+// activate event
 self.addEventListener('activate', evt => {
-  // console.log('service worker has been activated');
+  //console.log('service worker activated');
+  evt.waitUntil(
+    caches.keys().then(keys => {
+      //console.log(keys);
+      return Promise.all(keys
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
+        .map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
-// Fetch event
+// fetch event
 self.addEventListener('fetch', evt => {
-  // console.log('fetch event', evt);
+  //console.log('fetch event', evt);
   evt.respondWith(
     caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request);
+      return cacheRes || fetch(evt.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(evt.request.url, fetchRes.clone());
+          return fetchRes;
+        })
+      });
     })
   );
 });
