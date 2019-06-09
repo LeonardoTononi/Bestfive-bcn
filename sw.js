2fa16f6e11d75309174faabeba63811c84007136
Leonardo Tononi <easaaa@protonmail.com>
2019-06-09 19:24:16 +0200

    Added fallback pages and logic - Added limit cache

--- sw.js
+++ sw.js
@@ -13,6 +13,17 @@ const assets = [
   'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ];
 
+// Cache size limit function 
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
 // install event
 self.addEventListener('install', evt => {
   //console.log('service worker installed');
@@ -46,9 +57,14 @@ self.addEventListener('fetch', evt => {
       return cacheRes || fetch(evt.request).then(fetchRes => {
         return caches.open(dynamicCacheName).then(cache => {
           cache.put(evt.request.url, fetchRes.clone());
+          limitCacheSize(dynamicCacheName, 15);
           return fetchRes;
-        })
+        });
       });
+    }).catch(() => {
+      if (evt.request.url.indexOf('.html') > -1) {
+        caches.match('/en/fallback.html');
+      }
     })
   );
 });
