9378eb1efae131ecf9497b913693494b92048677
Leonardo Tononi <easaaa@protonmail.com>
2019-06-14 11:50:12 +0200

    Added logic show and hide to every place

--- javaScript/reviews.js
+++ javaScript/reviews.js
@@ -1,16 +1,60 @@
-const expandBtn_li = document.querySelector('.more');
-const expandImg_img = document.querySelector('.more-img');
-const pros_ul = document.querySelector('.pros');
-const cons_ul = document.querySelector('.cons');
+const expandBtn_img = document.querySelectorAll('.more-img');
+const pros_ul = document.querySelectorAll('.pros');
+const cons_ul = document.querySelectorAll('.cons');
 
-expandBtn_li.addEventListener('click', function () {
-  if (pros_ul.style.display == "none" & cons_ul.style.display == "none") {
-    expandImg_img.style.transform = "rotate(180deg)";
-    pros_ul.style.display = "inline-block";
-    cons_ul.style.display = "inline-block";
+expandBtn_img[0].addEventListener('click', function () {
+  if (pros_ul[0].style.display == "none" & cons_ul[0].style.display == "none") {
+    pros_ul[0].style.display = "inline-block";
+    cons_ul[0].style.display = "inline-block";
+    expandBtn_img[0].style.transform = "rotate(180deg)";
   } else {
-    expandImg_img.style.transform = "initial";
-    pros_ul.style.display = "none";
-    cons_ul.style.display = "none";
+    pros_ul[0].style.display = "none";
+    cons_ul[0].style.display = "none";
+    expandBtn_img[0].style.transform = "initial";
+  }
+});
+
+expandBtn_img[1].addEventListener('click', function () {
+  if (pros_ul[1].style.display == "none" & cons_ul[1].style.display == "none") {
+    pros_ul[1].style.display = "inline-block";
+    cons_ul[1].style.display = "inline-block";
+    expandBtn_img[1].style.transform = "rotate(180deg)";
+  } else {
+    pros_ul[1].style.display = "none";
+    cons_ul[1].style.display = "none";
+    expandBtn_img[1].style.transform = "initial";
+  }
+});
+expandBtn_img[2].addEventListener('click', function () {
+  if (pros_ul[2].style.display == "none" & cons_ul[2].style.display == "none") {
+    pros_ul[2].style.display = "inline-block";
+    cons_ul[2].style.display = "inline-block";
+    expandBtn_img[2].style.transform = "rotate(180deg)";
+  } else {
+    pros_ul[2].style.display = "none";
+    cons_ul[2].style.display = "none";
+    expandBtn_img[2].style.transform = "initial";
+  }
+});
+expandBtn_img[3].addEventListener('click', function () {
+  if (pros_ul[3].style.display == "none" & cons_ul[3].style.display == "none") {
+    pros_ul[3].style.display = "inline-block";
+    cons_ul[3].style.display = "inline-block";
+    expandBtn_img[3].style.transform = "rotate(180deg)";
+  } else {
+    pros_ul[3].style.display = "none";
+    cons_ul[3].style.display = "none";
+    expandBtn_img[3].style.transform = "initial";
+  }
+});
+expandBtn_img[4].addEventListener('click', function () {
+  if (pros_ul[4].style.display == "none" & cons_ul[4].style.display == "none") {
+    pros_ul[4].style.display = "inline-block";
+    cons_ul[4].style.display = "inline-block";
+    expandBtn_img[4].style.transform = "rotate(180deg)";
+  } else {
+    pros_ul[4].style.display = "none";
+    cons_ul[4].style.display = "none";
+    expandBtn_img[4].style.transform = "initial";
   }
 });
