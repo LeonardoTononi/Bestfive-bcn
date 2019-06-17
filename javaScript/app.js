0a6e046d2cea9059d1dc27594a43e74bba43e6ad
Leonardo Tononi <easaaa@protonmail.com>
2019-06-17 13:27:41 +0200

    Removed passive listeners

--- javaScript/app.js
+++ javaScript/app.js
@@ -3,7 +3,3 @@ if ('serviceWorker' in navigator) {
     .then((reg) => console.log('service worker registered', reg))
     .catch((err) => console.log('service worker not registered', err));
 }
-
-document.addEventListener('touchstart', onTouchStart, {
-  passive: true
-});
