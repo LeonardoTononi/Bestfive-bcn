744db0480cd28703dda59834419d4d1ded5ad9bc
Leonardo Tononi <easaaa@protonmail.com>
2019-07-02 20:48:44 +0200

    Added more questions in how js

--- javaScript/howto.js
+++ javaScript/howto.js
@@ -1,9 +1,51 @@
 const questions = [{
-  q: "How to arrive at the airport?",
-  a: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis accusantium et maiore impedit eveniet."
+  q: "How to buy ticket for Sagrada Familia?",
+  a: `Go to <a href="http://www.sagradafamilia.org/tickets" target="_blank">www.sagradafamilia.org</a>,  select your ticket preference and the date`
 }, {
-  q: "How to arrive at Placa Catalunya?",
-  a: "To arrive in Placa Catalunya you need to go straight until your legs are blooding."
+  q: "How to get to Sagrada Familia?",
+  a: `
+           <span class="option">Option 1:</span> take the metro <span class="metroL2">L2</span> or <span class="metroL5">L5</span> to Sagrada Familia stop. <br>
+           <span class="option">Option 2:</span> From plaza Catalunya take bus 50. <br>
+           <span class="option">Option 3:</span> From plaza Catalunya a Taxi is about 10 euros.`
+}, {
+  q: "How much costs a ticket for Sagrada Familia?",
+  a: `17€ Fast track ticket. <br>
+        25€ fast track ticket + audioguide. <br>
+        32€ fast track ticket + audioguide + one tower. <br>
+        40€ fast track ticket + tour guide. <br>
+        51€ fast track ticket + tour guide + one tower. <br>`
+}, {
+  q: "How to buy ticket for Park Guell?",
+  a: `Go to <a href="https://parkguell.barcelona/en/buy-tickets" target="_blank">www.parkguell.barcelona</a>, select your ticket preference and the date.`
+}, {
+  q: "How to get to Park Guell?",
+  a: ` 
+         <span class="option">Option 1:</span> Take bus 24 from plaza Catalunya.<br>
+         <span class="option">Option 2:</span> Take the metro <span class="metroL3">L3</span> to Vallcarca, walk down road and turn left to street Gloria and take the mechanical stairs that go up hill.<br>
+         <span class="option">Option 3:</span> From plaza Catalunya a Taxi is about 10 euros (20mins).`
+}, {
+  q: "How much is a ticket to Park Guell?",
+  a: "It's 10€ (7€ reduced, free for children 0-6 years old."
+}, {
+  q: "How much does the Picasso Museum ticket cost?",
+  a: `<span class="option">Collection:</span> 12€ (7€ reduced). <br>
+  <span class="option">Collection + temporary exhibiton:</span> 14 € (7,5 €reduced). <br>
+  <span class="option">Free entrance:</span> <br>
+  - Thursday afternoons, from 6.00pm to 9.30pm. <br>
+  - First Sunday of each month, from 9am to 8.30pm. <br>
+  Buy or book the free entrance at <a href="http://www.bcn.cat/museupicasso/en/visiting/planning.html" target="_blank">www.bcn.cat/museupicasso</a>`
+}, {
+  q: "Where can I buy tickets for the Picasso Museum?",
+  a: `Buy or book the free entrance at <a href="http://www.bcn.cat/museupicasso/en/visiting/planning.html" target="_blank">www.bcn.cat/museupicasso</a>`
+}, {
+  q: "When is the Barcelona Cathedral free entrance?",
+  a: `Between 8:30 and 12:45 every day except for Sundays (mass service). <br>From 12:45 to 17:45 it costs 7€.`
+}, {
+  q: "How much does the cable car to Montjuic cost?",
+  a: `There are 2 cable cars, one that takes you from the port to the mountain and another one that has 3 stops on the mountain with views of the city. <br>
+      <span class="option">Port cable car:</span> 11€ one way, 16,50€, way and back (buy tickets on counter with cash). <br>
+      <span class="option">Mountain cable car:</span> 12,70€ (you can purchase online with 20% discount). <br>
+      Buy ticket at <a href="https://www.holabarcelona.com/tickets/cable-car-montjuic-barcelona" target="_blank">www.holabarcelona.com</a>`
 }];
 
 function findMatches(wordToMatch, questions) {
