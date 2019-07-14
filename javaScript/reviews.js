38a890e24c44e0d93af50942f3d200e9ab66943c
Leonardo Tononi <easaaa@protonmail.com>
2019-07-14 13:34:19 +0200

    Added code inside html

--- javaScript/reviews.js
+++ javaScript/reviews.js
@@ -15,25 +15,17 @@ function effectImg(e) {
 
 // GET DIRECTIONS 
 
-const getDirection = document.querySelector('go_btn').href;
-
 const laPlatilleria = {
   lat: 41.371567,
   long: 2.165384
 }
 
-function getPlatilleria(lat, long) {
+/* function getDirection() {
   if ((navigator.platform.indexOf("iPhone") != -1) ||
     (navigator.platform.indexOf("iPad") != -1) ||
     (navigator.platform.indexOf("iPod") != -1)) {
     window.open("https://maps.google.com/maps?daddr=41.371562,2.165384&amp;ll=");
-    myWindow.close();
   } else {
     window.open("https://maps.google.com/maps?daddr=41.371562,2.165384&amp;ll=");
-    myWindow.close();
   }
-}
-
-/* function getPlatilleria() {
-  window.open("https://maps.google.com/maps?daddr=41.371562,2.165384&amp;ll=")
 } */
