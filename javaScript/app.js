e1f6d51756c6c2b3176191cefb4793b0b1027d2c
Leonardo Tononi <easaaa@protonmail.com>
2019-08-11 20:40:03 +0200

    Activated PWA

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,7 +1,6 @@
-/* if ('serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker
     .register('/sw.js')
     .then(reg => console.log('service worker registered', reg))
     .catch(err => console.log('service worker not registered', err));
 }
- */
