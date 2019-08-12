aea4f64e76b5b6c9537d6e6841eb2bd5a449599f
Leonardo Tononi <easaaa@protonmail.com>
2019-08-12 20:20:15 +0200

    Disactivated service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const staticCacheName = 'site-static:beta.01';
+/* const staticCacheName = 'site-static:beta.01';
 const dynamicCacheName = 'site-dynamic:beta.02';
 const assets = [
   '/',
@@ -98,3 +98,4 @@ self.addEventListener('fetch', evt => {
       })
   );
 });
+ */
