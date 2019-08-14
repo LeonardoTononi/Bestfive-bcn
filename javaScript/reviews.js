49e44554975baa6037946de290bf1bb78ec408b9
Leonardo Tononi <easaaa@protonmail.com>
2019-08-14 21:08:44 +0200

    Change position popup hours weekly

--- javaScript/reviews.js
+++ javaScript/reviews.js
@@ -5,10 +5,10 @@ expandBtn_img.forEach(img => img.addEventListener('click', effectImg));
 
 function effectImg(e) {
   this.classList.toggle('rotateImg');
-  this.nextElementSibling.style.visibility = "visible";
-  if (!this.classList.contains("rotateImg")) {
-    this.nextElementSibling.style.visibility = "hidden";
+  this.nextElementSibling.style.visibility = 'visible';
+  if (!this.classList.contains('rotateImg')) {
+    this.nextElementSibling.style.visibility = 'hidden';
   } else {
-    this.nextElementSibling.style.visibility = "visible";
+    this.nextElementSibling.style.visibility = 'visible';
   }
 }
