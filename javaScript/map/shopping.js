62013858af0d3b4299b70232c805d3718934808f
Leonardo Tononi <easaaa@protonmail.com>
2019-11-23 11:07:46 +0100

    Added shopping Category and add show more button for text descr

---
+++ javaScript/map/shopping.js
@@ -0,0 +1,235 @@
+// ==========================
+//         INIT MAP
+// ==========================
+function initMap() {
+  let options = {
+    zoom: 13,
+    center: {
+      lat: 41.3887,
+      lng: 2.1589
+    },
+    styles: [
+      {
+        featureType: 'poi',
+        elementType: 'labels.text',
+        stylers: [
+          {
+            visibility: 'off'
+          }
+        ]
+      },
+      {
+        featureType: 'poi.business',
+        stylers: [
+          {
+            visibility: 'off'
+          }
+        ]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.icon',
+        stylers: [
+          {
+            visibility: 'off'
+          }
+        ]
+      },
+      {
+        featureType: 'transit',
+        stylers: [
+          {
+            visibility: 'off'
+          }
+        ]
+      }
+    ]
+  };
+
+  // New map
+  let map = new google.maps.Map(document.getElementById('map'), options);
+
+  // Array of markers
+  let markers = [
+    // El Born
+    {
+      coords: {
+        lat: 41.384274,
+        lng: 2.18244
+      },
+      iconImage: {
+        url: '/img/icon/shopping.png',
+        scaledSize: new google.maps.Size(18, 18)
+      },
+      content: `<section class="maps-content">
+                     <p>Shopping</p>
+                     <h5>El Born</h5>
+                     <div class="go-btn">
+                        <a href="https://maps.google.com/maps?daddr=41.384274,2.182440&amp;ll=">GO!</a>
+                  </div>
+                </section>`
+    },
+    // El Raval
+    {
+      coords: {
+        lat: 41.381737,
+        lng: 2.168829
+      },
+      iconImage: {
+        url: '/img/icon/shopping.png',
+        scaledSize: new google.maps.Size(18, 18)
+      },
+      content: `<section class="maps-content">
+                  <p>Shopping</p>
+                  <h5>El Raval</h5>
+                  <div class="go-btn">
+                        <a href="https://maps.google.com/maps?daddr=41.381737,2.168829&amp;ll=">GO!</a>
+                  </div>
+                </section>`
+    },
+    // La Roca Village
+    {
+      coords: {
+        lat: 41.61016,
+        lng: 2.343559
+      },
+      iconImage: {
+        url: '/img/icon/shopping.png',
+        scaledSize: new google.maps.Size(18, 18)
+      },
+      content: `<section class="maps-content">
+                  <p>Shopping</p>
+                  <h5>La Roca Village</h5>
+                  <div class="go-btn">
+                        <a href="https://maps.google.com/maps?daddr=41.610160,2.343559&amp;ll=">GO!</a>
+                  </div>
+                </section>`
+    },
+    // Federal Cafe Parlament
+    {
+      coords: {
+        lat: 41.376802,
+        lng: 2.162845
+      },
+      iconImage: {
+        url: '/img/icon/shopping.png',
+        scaledSize: new google.maps.Size(18, 18)
+      },
+      content: `<section class="maps-content">
+                  <p>Shopping</p>
+                  <h5>Federal Cafe Parlament</h5>
+                  <div class="go-btn">
+                        <a href="https://maps.google.com/maps?daddr=41.376802,2.162845&amp;ll=">GO!</a>
+                  </div>
+                </section>`
+    },
+    // Paaseo De Gracia
+    {
+      coords: {
+        lat: 41.393219,
+        lng: 2.163756
+      },
+      iconImage: {
+        url: '/img/icon/shopping.png',
+        scaledSize: new google.maps.Size(18, 18)
+      },
+      content: `<section class="maps-content">
+                  <p>Shopping</p>
+                  <h5>Paaseo De Gracia</h5>
+                  <div class="go-btn">
+                        <a href="https://maps.google.com/maps?daddr=41.393219,2.163756&amp;ll=">GO!</a>
+                  </div>
+                </section>`
+    }, // SlowMov
+    {
+      coords: {
+        lat: 41.39817,
+        lng: 2.154821
+      },
+      iconImage: {
+        url: '/img/icon/shopping.png',
+        scaledSize: new google.maps.Size(18, 18)
+      },
+      content: `<section class="maps-content">
+                  <p>Shopping</p>
+                  <h5>SlowMov</h5>
+                  <div class="go-btn">
+                        <a href="https://maps.google.com/maps?daddr=41.398170,2.154821&amp;ll=">GO!</a>
+                  </div>
+                </section>`
+    }
+  ];
+
+  // ==========================
+  //         GET LIVE LOCATION
+  // ==========================
+
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(saveCoordinate);
+    } else {
+      // Nothing;
+    }
+  }
+
+  function saveCoordinate(position) {
+    markers.push({
+      coords: {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      },
+      iconImage: '',
+      zIndex: 999
+    });
+  }
+  getLocation();
+
+  // Loop through markers
+  setTimeout(function() {
+    for (let i = 0; i < markers.length; i++) {
+      addMarker(markers[i]);
+    }
+  }, 1000);
+  // Add marker function
+  function addMarker(props) {
+    let marker = new google.maps.Marker({
+      position: props.coords,
+      map: map,
+      icon: props.iconImage,
+      draggable: false,
+      zIndex: props.zIndex,
+      animation: google.maps.Animation.DROP
+    });
+    // Check content
+    if (props.content) {
+      let infoWindow = new google.maps.InfoWindow({
+        content: props.content
+      });
+      marker.addListener(
+        'click',
+        function() {
+          infoWindow.open(map, marker);
+        },
+        toggleBounce
+      );
+    }
+  }
+
+  function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+      marker.setAnimation(null);
+    } else {
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+  }
+}
+
+function mapsSelector() {
+  if (
+    navigator.platform.indexOf('iPhone') != -1 ||
+    navigator.platform.indexOf('iPad') != -1 ||
+    navigator.platform.indexOf('iPod') != -1
+  )
+    window.open('maps://maps.google.com/maps?daddr=<lat>,<long>&amp;ll=');
+  else window.open('https://maps.google.com/maps?daddr=<lat>,<long>&amp;ll=');
+}
--- javaScript/reviews.js
+++ javaScript/reviews.js
@@ -36,3 +36,35 @@ if (isRunningStandalone()) {
   const cardInstall = document.querySelector('.card-install-app');
   cardInstall.style.display = 'none';
 }
+
+// SHOW AND HIDE TEXT
+const showBtn = document.querySelectorAll('.show');
+const hideBtn = document.querySelectorAll('.hide');
+const fadeDiv = document.querySelectorAll('.fade');
+const text = document.querySelectorAll('.place_tips p');
+
+showBtn.forEach(btn => {
+  btn.addEventListener('click', showText);
+});
+
+hideBtn.forEach(btn => {
+  btn.addEventListener('click', hideText);
+});
+
+function showText(e) {
+  this.style.display = 'none';
+  this.nextElementSibling.style.display = 'block';
+  this.nextElementSibling.nextElementSibling.style.maxHeight = '100%';
+  this.nextElementSibling.nextElementSibling.nextElementSibling.style.marginTop =
+    '0';
+  this.nextElementSibling.nextElementSibling.nextElementSibling.style.height =
+    '30px';
+}
+
+function hideText(e) {
+  this.style.display = 'none';
+  this.previousElementSibling.style.display = 'block';
+  this.nextElementSibling.style.maxHeight = '130px';
+  this.nextElementSibling.nextElementSibling.style.marginTop = '-100px';
+  this.nextElementSibling.nextElementSibling.style.height = '100px';
+}
